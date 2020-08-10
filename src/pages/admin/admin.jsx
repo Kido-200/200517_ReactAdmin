e6940f41237e5a27e707d439a39c49693fd6fcdb
Kido_glb <goodMoring_pro@qq.com>
2020-08-10 09:50:37 +0800

    404

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -13,6 +13,7 @@ import User from '../user/user'
 import Bar from '../charts/bar'
 import Line from '../charts/line'
 import Pie from '../charts/pie'
+import NotFound from '../not-found/not-found'
 
 
 
@@ -36,6 +37,8 @@ export default class Admin extends Component{
               <Header>Header</Header>
               <Content style={{margin:20,backgroundColor:'#fff'}}>
                 <Switch>
+                  {/* 访问/ 会直接到home，访问其他不对应的会到NotFound */}
+                  <Redirect  exact={true} from='/' to='/home' />
                   <Route path='/home' component={Home}></Route>
                   <Route path='/category' component={Category}></Route>
                   <Route path='/product' component={Product}></Route>
@@ -44,7 +47,8 @@ export default class Admin extends Component{
                   <Route path='/charts/bar' component={Bar}></Route>
                   <Route path='/charts/line' component={Line}></Route>
                   <Route path='/charts/pie' component={Pie}></Route>
-                  <Redirect to='/home' />
+                  {/* 上面没一个匹配的直接显示 */}
+                  <Route component={NotFound} />
                 </Switch>
               </Content>
               <Footer style={{textAlign:'center',color:'#cccccc'}}>推荐使用谷歌浏览器，可以获得更佳的页面操作体验</Footer>
